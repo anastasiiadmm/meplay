2a97decfb40c004fc6f30fbadf04d766d176f0c1
Farid <thesomeq@gmail.com>
2021-01-18 12:25:46 +0600

    #5 Перенос библиотек из тестового проекта.

---
+++ lib/hexagon/hexagon_clipper.dart
@@ -0,0 +1,32 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import 'hexagon_type.dart';
+import 'utils.dart';
+
+class HexagonClipper extends CustomClipper<Path> {
+  HexagonClipper(this.type, {this.inBounds, this.borderRadius: 0});
+
+  final HexagonType type;
+  final bool inBounds;
+  final double borderRadius;
+
+  @override
+  Path getClip(Size size) {
+    return HexagonUtils.hexagonPath(
+      size,
+      type,
+      inBounds: inBounds,
+      borderRadius: borderRadius
+    );
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    if (oldClipper is HexagonClipper) {
+      return oldClipper.type != type;
+    }
+    return true;
+  }
+}
