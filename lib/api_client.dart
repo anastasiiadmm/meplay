dde24811baa99c89e2044ae22b7a9f8b769d3e45
Farid <thesomeq@gmail.com>
2021-03-06 01:05:13 +0600

    #20 Запрос в апи для сохранения токена приложения.

--- lib/api_client.dart
+++ lib/api_client.dart
@@ -1,9 +1,6 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:http/http.dart' as http;
-import 'package:device_info/device_info.dart';
-import 'package:flutter/services.dart';
-import 'package:crypto/crypto.dart';
 import 'dart:convert';
 import 'models.dart';
 
@@ -28,10 +25,11 @@ class ApiException implements Exception {
 class ApiClient {
   static Future<T> _wrapRequest<T>(
       Future<http.Response> Function() request,
-      T Function(http.Response) callback) async {
+      [T Function(http.Response) callback]) async {
     try {
       final response = await request();
       if (response.statusCode == 200) {
+        if (callback == null) return null;
         return callback(response);
       } else {
         throw ApiException('Ошибка при выполнении запроса');
@@ -152,7 +150,6 @@ class ApiClient {
       'password': user.password,
       'packet_id': packet.id.toString(),
     };
-    print(body);
     return _wrapRequest<List<Packet>>(
       () =>  http.post(url, body: body),
       (response) {
@@ -173,73 +170,15 @@ class ApiClient {
     );
   }
 
-  static Future<User> authOld(String username, String password) async {
-    final url = '$BASE_API_URL/stalker_portal/auth/token';
-    final timestamp = DateTime.now().millisecondsSinceEpoch;
-    var deviceData = await _getDeviceData();
-    var signature = _getSignature(deviceData['id']);
+  static Future<void> saveFirebaseToken(String token, [User user]) async {
+    user ??= await User.getUser();
+    final url = '$BASE_API_URL/stalker_portal/meplay/fcm_token';
     final body = {
-      'grant_type': 'password',
-      'username': username,
-      'password': password,
-      'client_type': 'Robot',
-      'model': deviceData['model'],
-      'hw_version': _getHWId(deviceData['model'], signature, timestamp ~/ 1000),
-      'signature': signature,
-      'timestamp': (timestamp).toString(),
+      'token': token,
+      'user_id': user?.id,
     };
-    return _wrapRequest<User>(
-      () => http.post(url, body: body),
-      (response) {
-        var data = jsonDecode(response.body);
-        return User.fromJson(data);
-      }
+    return _wrapRequest<void>(
+      () =>  http.post(url, body: body),
     );
-  }
-
-  static String _getSignature(deviceId) {
-    var data = utf8.encode('$deviceId');
-    var hash = sha1.convert(data).toString();
-    return hash;
-  }
-
-  static String _getHWId(String model, String signature, int timestamp) {
-    var key = utf8.encode(signature);
-
-    var hmacSha1 = new Hmac(sha1, key);
-    var hmacMd5 = new Hmac(md5, key);
-
-    var sha1Data = utf8.encode(model);
-    var sha1Hash = hmacSha1.convert(sha1Data).toString();
-
-    var md5Data = utf8.encode(model);
-    var md5Hash = hmacMd5.convert(md5Data).toString();
-
-    var hash = '$sha1Hash.$md5Hash.$timestamp';
-    return hash;
-  }
-
-  static Future<Map<String, String>> _getDeviceData() async {
-    final deviceData = <String, String>{};
-
-    final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-
-    try {
-      if (Platform.isAndroid) {
-        final AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
-        deviceData['model'] = androidInfo.model;
-        deviceData['id'] = androidInfo.androidId;
-      } else if (Platform.isIOS) {
-        final IosDeviceInfo iosDeviceInfo = await deviceInfo.iosInfo;
-        deviceData['model'] = iosDeviceInfo.model;
-        deviceData['id'] = iosDeviceInfo.identifierForVendor;
-      }
-    } on PlatformException {
-      deviceData['Error'] = 'Failed to get platform version.';
-      deviceData['model'] = null;
-      deviceData['id'] = null;
-    }
-
-    return deviceData;
   }
 }
