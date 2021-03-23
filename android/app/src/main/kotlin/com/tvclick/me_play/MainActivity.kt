997bc6c4dc9bd0624e02c0a3dbea68dc6aa22d6c
Farid <thesomeq@gmail.com>
2021-03-23 23:42:48 +0600

    #13. Фиксы настроек для хромкаста под андроид. Хромкаст работает.

--- android/app/src/main/kotlin/com/tvclick/me_play/MainActivity.kt
+++ android/app/src/main/kotlin/com/tvclick/me_play/MainActivity.kt
@@ -2,11 +2,11 @@ package com.tvclick.meplay_2
 
 import com.google.android.gms.cast.framework.CastContext
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         CastContext.getSharedInstance(applicationContext)
