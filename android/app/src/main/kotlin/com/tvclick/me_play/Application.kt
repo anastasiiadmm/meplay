511e776e96a2af0d6d7a2912e9dc8f6d7c85412f
Farid <thesomeq@gmail.com>
2021-04-19 11:20:19 +0600

    #53 Заготовка для подключения яндекс-метрики под андроид.

--- android/app/src/main/kotlin/com/tvclick/me_play/Application.kt
+++ android/app/src/main/kotlin/com/tvclick/me_play/Application.kt
@@ -3,9 +3,20 @@ package com.tvclick.meplay_2
 import android.content.Context
 import io.flutter.app.FlutterApplication
 import androidx.multidex.MultiDex
+import com.yandex.metrica.YandexMetrica
+import com.yandex.metrica.YandexMetricaConfig
 
+
+const val YM_API_KEY = ""  // TODO
 
 class Application: FlutterApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        val config = YandexMetricaConfig.newConfigBuilder(YM_API_KEY).build()
+        YandexMetrica.activate(applicationContext, config)
+        YandexMetrica.enableActivityAutoTracking(this)
+    }
+
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
         MultiDex.install(this)
