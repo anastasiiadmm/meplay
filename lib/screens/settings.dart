0ebc5d41fd4eb1f99a62e0526b63b70c7931f853
Farid <thesomeq@gmail.com>
2021-05-26 11:05:35 +0600

    #41. Классы настроек вынесены в отдельный файл.

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,48 +1,8 @@
 import 'package:flutter/material.dart';
 import '../widgets/bottomNavBar.dart';
 import '../utils/pref_helper.dart';
+import '../utils/settings.dart';
 import '../theme.dart';
-
-
-class ListType {
-  final String name;
-  final String value;
-
-  const ListType(this.name, this.value);
-
-  static const hexes = ListType('Шестиугольники', 'hexes');
-  static const list = ListType('Список', 'list');
-  static const blocks = ListType('Плитка', 'tiles');
-  static const choices = [hexes, list, blocks];
-  static const defaultChoice = hexes;
-
-  static ListType getByName(String name) {
-    for (ListType choice in choices) {
-      if(choice.name == name) return choice;
-    }
-    return defaultChoice;
-  }
-}
-
-
-class Language {
-  final String name;
-  final Locale value;
-
-  const Language(this.name, this.value);
-
-  static const ru = Language('Русский', Locale('ru', 'RU'));
-  static const ky = Language('Кыргызча', Locale('ky', 'KG'));
-  static const choices = [ky, ru];
-  static const defaultChoice = ru;
-
-  static Language getByName(String name) {
-    for (Language choice in choices) {
-      if(choice.name == name) return choice;
-    }
-    return defaultChoice;
-  }
-}
 
 
 class SettingsScreen extends StatefulWidget {
