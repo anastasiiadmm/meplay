83e21688f15071fa0d2b79effb13be033698497a
Farid <thesomeq@gmail.com>
2021-05-31 11:31:07 +0600

    #65 Плитка канала переделана в stateless widget, т.к. у неё нет своего стейта.

--- lib/widgets/channel_tile.dart
+++ lib/widgets/channel_tile.dart
@@ -4,26 +4,21 @@ import '../theme.dart';
 import 'channel_logo.dart';
 
 
-class ChannelTile extends StatefulWidget {
+class ChannelTile extends StatelessWidget {
   final Channel channel;
 
   ChannelTile({Key key, @required this.channel}): super(key: key);
 
-  @override
-  _ChannelTileState createState() => _ChannelTileState();
-}
-
-class _ChannelTileState extends State<ChannelTile> {
   Widget get _title {
     return Text(
-      widget.channel.title,
+      channel.title,
       style: AppFontsV2.itemTitle,
     );
   }
 
   Widget get _program {
     return FutureBuilder(
-      future: widget.channel.currentProgram,
+      future: channel.currentProgram,
       builder: (BuildContext context, AsyncSnapshot<Program> snapshot) {
         return Padding(
           padding: EdgeInsets.only(top: 4),
@@ -47,19 +42,19 @@ class _ChannelTileState extends State<ChannelTile> {
     return PreferredSize(
       preferredSize: Size(double.infinity, 91),
       child: GestureDetector(
-        onTap: () => _openChannel(context, widget.channel),
+        onTap: () => _openChannel(context, channel),
         child: Row(
           children: [
             Padding(
               padding: EdgeInsets.fromLTRB(0, 0, 16, 8),
-              child: ChannelLogo(channel: widget.channel),
+              child: ChannelLogo(channel: channel),
             ),
             Expanded(
               child: DecoratedBox(
                 decoration: BoxDecoration(
                   border: Border(
                     bottom: BorderSide(color: AppColorsV2.decorativeGray),
-                  )
+                  ),
                 ),
                 child: SizedBox(
                   height: 91,
@@ -83,4 +78,3 @@ class _ChannelTileState extends State<ChannelTile> {
     ) ;
   }
 }
-
