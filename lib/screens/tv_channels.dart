1b85e08beec85c612155b09af9c6763feea5126b
Farid <thesomeq@gmail.com>
2021-05-31 13:14:50 +0600

    #68 Старый список каналов переименован в channel_list_old.dart, чтобы не путать с новым.

--- lib/screens/tv_channels.dart
+++ lib/screens/tv_channels.dart
@@ -6,7 +6,7 @@ import '../widgets/modals.dart';
 import '../models.dart';
 import '../theme.dart';
 import '../utils/pref_helper.dart';
-import '../widgets/channel_list_max.dart';
+import '../widgets/channel_list_old.dart';
 import 'player.dart';
 
 
