272263dd29c7b491355c03a4ba3d43ca245c5fc9
Farid <thesomeq@gmail.com>
2021-06-24 11:26:45 +0600

    #34 Закомментированы все лишние и нерабочие сервисы, из-за которых приложение не запускается.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,8 +1,8 @@
 import UIKit
 import Flutter
 import GoogleCast
-import YandexMobileMetrica
-import FBSDKCoreKit
+//import YandexMobileMetrica
+//import FBSDKCoreKit
 
 
 let YMApiKey = ""
@@ -10,6 +10,7 @@ let YMApiKey = ""
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate, GCKLoggerDelegate {
+  // flutter_video_cast
   let kReceiverAppID = kGCKDefaultMediaReceiverApplicationID
   let kDebugLoggingEnabled = true
 
@@ -17,44 +18,52 @@ let YMApiKey = ""
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    if #available(iOS 13.0, *) { } else {
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        let mainViewController = UIViewController()
-        let mainNavigationController = UINavigationController(rootViewController: mainViewController)
-        self.window!.rootViewController = mainNavigationController
-        self.window!.makeKeyAndVisible()
-    }
+    // FaceBook SDK?
+//    if #available(iOS 13.0, *) { } else {
+//        self.window = UIWindow(frame: UIScreen.main.bounds)
+//        let mainViewController = UIViewController()
+//        let mainNavigationController = UINavigationController(rootViewController: mainViewController)
+//        self.window!.rootViewController = mainNavigationController
+//        self.window!.makeKeyAndVisible()
+//    }
 
+    // flutter default
     GeneratedPluginRegistrant.register(with: self)
+    
+    // flutter_vido_cast
     let criteria = GCKDiscoveryCriteria(applicationID: kReceiverAppID)
     let options = GCKCastOptions(discoveryCriteria: criteria)
     GCKCastContext.setSharedInstanceWith(options)
     GCKCastContext.sharedInstance().useDefaultExpandedMediaControls = true
     GCKLogger.sharedInstance().delegate = self
 
+    // Yandex Metrica
 //    let configuration = YMMYandexMetricaConfiguration.init(apiKey: YMApiKey)
 //    YMMYandexMetrica.activate(with: configuration!)
 
-    ApplicationDelegate.shared.application(
-        application,
-        didFinishLaunchingWithOptions: launchOptions
-    )
+    // FaceBook SDK
+//    ApplicationDelegate.shared.application(
+//        application,
+//        didFinishLaunchingWithOptions: launchOptions
+//    )
 
+    // iOS default
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
-  override func application(
-      _ app: UIApplication,
-      open url: URL,
-      options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-  ) -> Bool {
-    ApplicationDelegate.shared.application(
-      app,
-      open: url,
-      sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
-      annotation: options[UIApplication.OpenURLOptionsKey.annotation]
-    )
-
-    return true
-  }
+//  override func application(
+//      _ app: UIApplication,
+//      open url: URL,
+//      options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+//  ) -> Bool {
+//    // FaceBook SDK
+//    ApplicationDelegate.shared.application(
+//      app,
+//      open: url,
+//      sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+//      annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+//    )
+//
+//    return true
+//  }
 }
