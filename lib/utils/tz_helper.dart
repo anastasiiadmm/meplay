07ca6e2047a3695d5cdb1c35d8044bb4d99de1ac
Farid <thesomeq@gmail.com>
2021-09-08 15:28:33 +0600

    #89 Конверсия временных зон в формате GMT в Etc/GMT.

--- lib/utils/tz_helper.dart
+++ lib/utils/tz_helper.dart
@@ -1,13 +1,20 @@
-import 'package:timezone/data/latest.dart' as tzInfo;
-import 'package:timezone/timezone.dart';
 import 'package:flutter_native_timezone/flutter_native_timezone.dart';
-
+import 'package:timezone/data/latest_all.dart' as tzInfo;
+import 'package:timezone/timezone.dart';
 
 class TZHelper {
   static Future<void> init() async {
-    String localZone = await FlutterNativeTimezone.getLocalTimezone();
-
     tzInfo.initializeTimeZones();
+
+    String localZone = await FlutterNativeTimezone.getLocalTimezone();
+    if (localZone.startsWith('GMT')) {
+      String zoneName = 'Etc/GMT' + localZone[3];
+      if (localZone[4] != '0') {
+        zoneName += localZone[4];
+      }
+      zoneName += localZone[5];
+      localZone = zoneName;
+    }
     setLocalLocation(getLocation(localZone));
   }
 
